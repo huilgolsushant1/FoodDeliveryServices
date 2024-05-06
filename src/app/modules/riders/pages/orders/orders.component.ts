1fe73d0260a0e28514c1e86d74ec7317cddfd784
vindhyahegde <vindhyahegde2114@gmail.com>
2024-05-06 13:36:44 +0200

    riders

--- src/app/modules/riders/pages/orders/orders.component.ts
+++ src/app/modules/riders/pages/orders/orders.component.ts
@@ -27,16 +27,16 @@ export class OrdersComponent implements OnInit{
   ngOnInit() {
     Marker.prototype.options.icon = this.defaultIcon;
 
-    this.map = L.map("map").setView([51.505, -0.09], 13);
+    this.map = L.map("map").setView([37.563434, -122.322255], 13);
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-    L.Routing.control({
-      waypoints: [L.latLng(57.74, 11.94), L.latLng(57.6792, 11.949)],
-      routeWhileDragging: true
-    }).addTo(this.map);
+    // L.Routing.control({
+    //   waypoints: [L.latLng(57.74, 11.94), L.latLng(57.6792, 11.949)],
+    //   routeWhileDragging: true
+    // }).addTo(this.map);
   }
 }
