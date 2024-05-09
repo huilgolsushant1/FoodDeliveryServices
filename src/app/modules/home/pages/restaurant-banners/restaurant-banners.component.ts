831b24f506216458d2c7f5c53271dc4e7080bc86
ksard <114796849+ksard@users.noreply.github.com>
2024-05-09 13:43:51 +0200

    added getCuisineRestaurantsController.js, topBudgetRestaurantsController.js, topRatedRestaurantsController.js, updated topRated.js router, neo4j.service.ts, server.js updated restaurante-banners.component.html, restaurant-banners.component.ts

--- src/app/modules/home/pages/restaurant-banners/restaurant-banners.component.ts
+++ src/app/modules/home/pages/restaurant-banners/restaurant-banners.component.ts
@@ -1,30 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Neo4jService } from '../../../../neo4j.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Neo4jService } from "../../../../neo4j.service";
+import { take } from "rxjs/operators";
+import { combineLatest } from "rxjs";
 
 @Component({
-  selector: 'app-restaurant-banners',
-  templateUrl: './restaurant-banners.component.html',
-  styleUrls: ['./restaurant-banners.component.css']
+  selector: "app-restaurant-banners",
+  templateUrl: "./restaurant-banners.component.html",
+  styleUrls: ["./restaurant-banners.component.css"],
 })
 export class RestaurantBannersComponent implements OnInit {
-  top5Influencer: any[] = [];
+  topInfluencer: any[] = [];
+  topRestaurants: any[] = [];
+  topCuisines: any[] = [];
+  topSelectedCuisine: any[] = [];
+  topBudgetFriendly: any[] = [];
 
   constructor(private neo4jService: Neo4jService, private router: Router) {}
 
   ngOnInit() {
-    this.neo4jService.getData().subscribe(
-      (data) => {
-        this.top5Influencer = data;
-        console.log("Data from Neo4j:", this.top5Influencer);
-      },
-      (error) => {
-        console.error("Error fetching data from Neo4j:", error);
-      }
-    );
+    combineLatest([
+      this.neo4jService.getInfluencerPicks(),
+      this.neo4jService.getTopRestaurants(),
+      this.neo4jService.getTopCuisines(),
+      this.neo4jService.getTopBudgetFriendly(),
+    ])
+      .pipe(take(1))
+      .subscribe(([influencers, restaurants, cuisines, budget]) => {
+        this.topInfluencer = influencers;
+        this.topRestaurants = restaurants;
+        this.topCuisines = cuisines;
+        this.topSelectedCuisine = this.topCuisines[0].topRestaurants;
+        this.topBudgetFriendly = budget;
+      });
+    // this.neo4jService.getInfluencerPicks().subscribe(
+    //   (data) => {
+    //     this.topInfluencer = data;
+    //     console.log("Data from Neo4j:", this.topInfluencer);
+    //   },
+    //   (error) => {
+    //     console.error("Error fetching data from Neo4j:", error);
+    //   }
+    // );
   }
 
   onRowClick(restaurant: any) {
-    this.router.navigate(['/details'], { state: { restaurant } });
+    this.router.navigate(["/details"], { state: { restaurant } });
+  }
+
+  onCuisineChange(selectedCuisine: any): void {
+    if (selectedCuisine.target && selectedCuisine.target.value) {
+      this.topSelectedCuisine = this.topCuisines.find(
+        ({ cuisine }) => cuisine === selectedCuisine.target.value
+      )?.topRestaurants;
+    }
   }
 }
